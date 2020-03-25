294c3fdb3db0277fa44f1b2a9d22530cb2a1e871
Maria Lagunero <maria58@gmail.com>
2020-03-25 14:40:25 -0700

    add debugger and console lot to check for errors in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
   $("#blanks form").submit(function() {
-    alert('Got to beginning of form submit!');
+    
     var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
     blanks.forEach(function(blank) {
-      var userInput = $("input." + blank).val();
+      var userInput = $("input#" + blank).val();
+      console.log(userInput);
       $("." + blank).text(userInput).val();      
     });
 
     $("#story").show();
-    alert('Got to end of form submit!');
+    
 
     event.preventDefault();
   });
